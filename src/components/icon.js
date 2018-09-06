6eb3bbc7fb239ae4d31fe57862b6e48547a3f036
Lauren <lmnishizaki@gmail.com>
2018-09-06 16:50:27 -0700

    Enable deploying to github pages

--- src/components/icon.js
+++ src/components/icon.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Icon(iconName, optClass) {
   const className = (optClass === undefined) ? 'Icon' : ('Icon ' + optClass);
-  const pathName = '/static/feather-sprite.svg#' + iconName;
+  const pathName = './static/feather-sprite.svg#' + iconName;
   return (
     <svg className={className}>
       <use xlinkHref={pathName}/>
