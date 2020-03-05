cc426cee2a9800922e7249ada3b0d652b7f33b18
Lauren <lmnishizaki@gmail.com>
2020-03-05 02:37:09 -0800

    Updates

---
+++ precache-manifest.afc1e7f71b94f5013a4096acbc22b610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6bb923f7659f433a69b162c8c785190",
+    "url": "/trois/index.html"
+  },
+  {
+    "revision": "6f435515c0fb3eca90e1",
+    "url": "/trois/static/css/main.da9b9e41.chunk.css"
+  },
+  {
+    "revision": "d1d007aaef74070ae754",
+    "url": "/trois/static/js/2.fc488774.chunk.js"
+  },
+  {
+    "revision": "42a9995508aaf43245daf09d0f412b11",
+    "url": "/trois/static/js/2.fc488774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f435515c0fb3eca90e1",
+    "url": "/trois/static/js/main.db43e120.chunk.js"
+  },
+  {
+    "revision": "5db7fa94a46be57e2dbd",
+    "url": "/trois/static/js/runtime-main.69520b50.js"
+  }
+]);
